9bab0d24f202fb0405480cf8c70b00bb3e9b1b6d
ngayMaivanden <datlai304@gmail.com>
2024-03-22 17:12:07 +0700

    build

--- dist/services/shop.service.js
+++ dist/services/shop.service.js
@@ -11,7 +11,6 @@ const order_model_1 = require("../models/order.model");
 const product_model_1 = __importDefault(require("../models/product.model"));
 const shop_model_1 = require("../models/shop.model");
 const user_model_1 = __importDefault(require("../models/user.model"));
-const shop_repository_1 = __importDefault(require("../repositories/shop.repository"));
 const notification_util_1 = require("../utils/notification.util");
 const uploadCloudinary_1 = __importDefault(require("../utils/uploadCloudinary"));
 class ShopService {
@@ -188,19 +187,23 @@ class ShopService {
                 product_is_bought: 1
             }
         };
-        const foundOrder = await shop_repository_1.default.getMyOrderShop({
-            shop_id: new mongoose_1.Types.ObjectId(shop_id),
-            limit: LIMIT,
-            skip: SKIP
-        });
-        // const result = await orderModel
-        //       .find({ 'order_products.products.shop_id': new Types.ObjectId(shop_id as string) })
-        //       .populate({
-        //             path: 'order_products.products.product_id',
-        //             model: 'Product',
-        //             select: '_id product_thumb_image product_name product_votes product_price'
-        //       })
-        //       .exec()
+        // const foundOrder = await ShopRepository.getMyOrderShop({
+        //       shop_id: new Types.ObjectId(shop_id as string),
+        //       limit: LIMIT,
+        //       skip: SKIP
+        // })
+        const result = await order_model_1.orderModel
+            .find({ 'order_products.products.shop_id': new mongoose_1.Types.ObjectId(shop_id) })
+            .populate({
+            path: 'order_products.products',
+            model: 'Cart',
+            populate: {
+                path: 'order_product.products.product_id',
+                model: 'Product',
+                select: '_id product_thumb_image product_name product_votes product_price'
+            }
+        })
+            .exec();
         // const foundOrder = await orderModel
         //       .find(orderQuery)
         //       .populate({
@@ -214,7 +217,7 @@ class ShopService {
         // const end = start + LIMIT
         // const pagination = foundOrder?.order_products.slice(start, end)
         // console.log({ start, end })
-        return { orderShop: foundOrder || { order_products: [] } };
+        return { orderShop: result || { order_products: [] } };
     }
     static async getShopAdmin(req) {
         const admin = await user_model_1.default.findOne({ roles: 'admin' });
