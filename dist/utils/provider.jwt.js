4b9a7ca46e190173a5f2c00bc96f24d774901eec
ngayMaivanden <datlai304@gmail.com>
2024-03-25 14:39:41 +0700

    fix authentication

--- dist/utils/provider.jwt.js
+++ dist/utils/provider.jwt.js
@@ -7,7 +7,7 @@ const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 class ProviderJWT {
     static createPairToken({ payload, key }) {
         try {
-            const access_token = jsonwebtoken_1.default.sign(payload, key.public_key, { expiresIn: '1d' });
+            const access_token = jsonwebtoken_1.default.sign(payload, key.public_key, { expiresIn: '15s' });
             const refresh_token = jsonwebtoken_1.default.sign(payload, key.private_key, { expiresIn: '7d' });
             // console.log({ refresh_token, access_token })
             return { access_token, refresh_token };
